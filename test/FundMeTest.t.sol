f264fa86c0617557d8cdd139ba672a2903f5afac
Gifftybabe <ulokangozi@gmail.com>
2024-12-14 08:05:33 +0100

    gas optimization

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,13 +6,13 @@ import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
-
 contract FundMeTest is Test {
     FundMe fundMe;
 
     address USER = makeAddr("user");
-    uint256 constant SEND_VALUE = 0.1 ether; 
+    uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    
 
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -26,40 +26,132 @@ contract FundMeTest is Test {
     }
 
     function testContractOwner() public view {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
-
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
 
-function testFundFailsWithoutEnoughEth() public {
-    // Expect the transaction to revert due to insufficient funds
-    vm.expectRevert(); // This indicates the next line is expected to fail
-    fundMe.fund(); // Send 0 value
+    function testFundFailsWithoutEnoughEth() public {
+        // Expect the transaction to revert due to insufficient funds
+        vm.expectRevert(); // This indicates the next line is expected to fail
+        fundMe.fund(); // Send 0 value
 
-  // Expect the custom error to be triggered
-    // vm.expectRevert(FundMe_NotEnoughEth.selector); 
-    // fundMe.fund{value: 0}(); // Send 0 value
+        // Expect the custom error to be triggered
+        // vm.expectRevert(FundMe_NotEnoughEth.selector);
+        // fundMe.fund{value: 0}(); // Send 0 value
 
-//     function testFundFailsBelowMinimumEth() public {
-//     // Expect the custom error for insufficient funds
-//     vm.expectRevert(FundMe_NotEnoughEth.selector);
-//     fundMe.fund{value: 4e18}(); // Send less than the minimum value
-// }
+        //     function testFundFailsBelowMinimumEth() public {
+        //     // Expect the custom error for insufficient funds
+        //     vm.expectRevert(FundMe_NotEnoughEth.selector);
+        //     fundMe.fund{value: 4e18}(); // Send less than the minimum value
+        // }
+    }
 
-}
+    function testFundUpdatesFundedDataSgtructure() public {
+        vm.prank(USER); // The next tx will be sent by USER
 
-function testFundUpdatesFundedDataSgtructure() public {
-    vm.prank(USER); // The next tx will be sent by USER
+        fundMe.fund{value: SEND_VALUE}();
 
-    fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
 
-    uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
-    assertEq(amountFunded, SEND_VALUE);
-}
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+      
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        // Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // Arrange
+
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1; // We start the address index from 1 because sometimes address 0 reverts and doesn't allopw stuffs like that because it is been checked when writing tests.
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            // address()
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+          uint256 startingOwnerBalance = fundMe.getOwner().balance;
+          uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+
+    }
 
 
+
+     function testWithdrawFromMultipleFundersCheaper() public funded {
+        // Arrange
+
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1; // We start the address index from 1 because sometimes address 0 reverts and doesn't allopw stuffs like that because it is been checked when writing tests.
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            // address()
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+          uint256 startingOwnerBalance = fundMe.getOwner().balance;
+          uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+
+    }
 }
