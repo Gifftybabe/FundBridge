ad703ea41e036d515823f607f2ac626d3f94c159
Gifftybabe <ulokangozi@gmail.com>
2024-12-14 09:38:59 +0100

    integration logic

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe} from "../../script/Interactions.s.sol";
+
+contract InterationsTest is Test {
+     FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    
+
+    function setUp() external {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+}
